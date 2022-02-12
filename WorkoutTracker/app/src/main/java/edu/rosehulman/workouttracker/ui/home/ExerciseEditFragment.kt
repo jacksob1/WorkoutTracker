61bd49c1d0ff58de9312dfde4bdfcc0f73ef3e8a
jacksob1 <jacksob1@rose-hulman.edu>
2022-02-11 19:50:28 -0500

    fixing bug with updating exercise and not changing the name

--- WorkoutTracker/app/src/main/java/edu/rosehulman/workouttracker/ui/home/ExerciseEditFragment.kt
+++ WorkoutTracker/app/src/main/java/edu/rosehulman/workouttracker/ui/home/ExerciseEditFragment.kt
@@ -1,6 +1,7 @@
 package edu.rosehulman.workouttracker.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -28,20 +29,23 @@ class ExerciseEditFragment : Fragment() {
         model.getExerciseNames()
         binding = FragmentExerciseFormBinding.inflate(inflater, container, false)
 
+        updateView()
+
         setupListeners()
+
         val items = model.getChoices()
         val adapter = ArrayAdapter(requireActivity(), R.layout.drop_down_item, items)
         binding.pickerTextView.setAdapter(adapter)
-        updateView()
         return binding.root
     }
 
     fun setupListeners() {
         binding.doneButton.setOnClickListener {
             var exerciseName = binding.pickerTextView.text.toString()
+            Log.d("WT", "Exercise: $exerciseName")
             var setsText = binding.exerciseSetsValue.text.toString()
             var repsText = binding.exerciseRepsValue.text.toString()
-            exerciseName = if(exerciseName.isNotBlank()){exerciseName} else {"Exercise"}
+            exerciseName = if(exerciseName.isNotBlank()){exerciseName} else {model.getCurrentExercise().name}
             var sets = if(setsText.isNotBlank()){if(setsText.toInt() > 0){setsText.toInt()} else {0}} else {model.getCurrentExercise().sets}
             var reps = if(repsText.isNotBlank()){if(repsText.toInt() > 0){repsText.toInt()} else {0}} else {model.getCurrentExercise().reps}
             var notes = binding.exerciseNotesValue.text.toString()
